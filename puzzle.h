82a886044107d72efa11cefc4391aa7c367d41d3
Julieanne Nguyen <jnguy780@ucr.edu>
2025-02-03 14:56:09 -0800

    header file updated for solving type

--- puzzle.h
+++ puzzle.h
@@ -1,6 +1,12 @@
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
 #include <vector>
 #include <string>
+
 using namespace std;
+
+const vector<vector<int>> GOAL = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}}; //define goal state
 
 struct PuzzleState{
     vector<vector<int>> puzzle;
@@ -17,3 +23,19 @@ struct PuzzleState{
         return puzzle == other.puzzle;                  //compare for goal
     }
 };
+
+PuzzleState customPuzzle(); //user built puzzle
+
+void uniformCostSearch(const PuzzleState& initstate);       //option 1 selected
+
+void misplacedTile(const PuzzleState& initstate);       //option 2 selected
+int mispplacedTileFind(const vector<vector<int>> puzzle); //find num of misplaced tiles
+
+void manhattanDistance(const PuzzleState& initstate);       //option 3 selected
+int manhattanDistanceFind(const vector<vector<int>> puzzle); //calculate manhattan distance
+
+bool isGoalState(const PuzzleState& state);     //compare curr state to goal
+
+vector<PuzzleState> moves(const PuzzleState& state);
+
+#endif
